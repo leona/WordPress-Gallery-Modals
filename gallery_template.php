810ce395861b184fb36b032051c9fa93152afd2f
Leon Harvey <leonharvey@live.com>
2015-05-28 10:49:12 +0100

    Update

--- gallery_template.php
+++ gallery_template.php
@@ -1,29 +1,29 @@
 <style type='text/css'>
-	#gallery-<?=$instance?> {
+	#gallery-<?php echo $instance; ?> {
 		margin: auto;
 	}
-	#gallery-<?=$instance?> .gallery-item {
+	#gallery-<?php echo $instance; ?> .gallery-item {
 		float: left;
 		margin-top: 10px;
 		text-align: center;
 		width: 33%;
 	}
-	#gallery-<?=$instance?> img {
+	#gallery-<?php echo $instance; ?> img {
 		border: 2px solid #cfcfcf;
 	}
-	#gallery-<?=$instance?> .gallery-caption {
+	#gallery-<?php echo $instance?> .gallery-caption {
 		margin-left: 0;
 	}
 </style>
 
-<div id='gallery-<?=$instance?>' class='gallery galleryid-<?=$gallery_atts['id']?> gallery-columns-3 gallery-size-thumbnail'>
+<div id='gallery-<?php echo $instance; ?>' class='gallery galleryid-<?php echo $gallery_atts['id']; ?> gallery-columns-3 gallery-size-thumbnail'>
 	<?php foreach($images as $image) { ?>
 	<dl class='gallery-item'>
 		<dt class='gallery-icon landscape'>
-			<a href='#'><img width="150" height="150" data-width="<?=$image->guid_dimensions[0]?>" data-height="<?=$image->guid_dimensions[1]?>" src="<?=$image->thumbnail?>" data-full-image="<?=$image->guid?>" class="attachment-thumbnail" alt="Test image" aria-describedby="gallery-<?=$instance?>-<?=$image->ID?>" /></a>
+			<a href='#'><img width="150" height="150" data-width="<?php echo $image->guid_dimensions[0]; ?>" data-height="<?php echo $image->guid_dimensions[1]; ?>" src="<?php echo $image->thumbnail?>" data-full-image="<?php echo $image->guid; ?>" class="attachment-thumbnail" alt="Test image" aria-describedby="gallery-<?php echo $instance?>-<?=$image->ID; ?>" /></a>
 		</dt>
-		<dd class='wp-caption-text gallery-caption' id='gallery-<?=$instance?>-<?=$image->ID?>'>
-		<?=$image->post_excerpt?>
+		<dd class='wp-caption-text gallery-caption' id='gallery-<?php echo $instance?>-<?php echo $image->ID; ?>'>
+		<?php echo $image->post_excerpt?>
 		</dd>
 	</dl>
 	<?php } ?>
